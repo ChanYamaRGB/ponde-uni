9eebedf26af9201a7beaec0146c7982613785f0c
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2025-07-30 17:42:10 +0900

    Update uni-string.mjs

--- app/commands/uni/uni-string.mjs
+++ app/commands/uni/uni-string.mjs
@@ -1,12 +1,32 @@
 import { SlashCommandBuilder } from 'discord.js';
+import blackboardMap from './DB/blackboard.mjs';
+import boldMap from './DB/bold.mjs';
 import cursiveMap from './DB/cursive.mjs';
+import frakturMap from './DB/fraktur.mjs';
+import mincho from './DB/mincho.mjs';
+import typewriterMap from './DB/typewriter.mjs';
 
 // 変換処理一覧
+const blackboardConvert = (str) =>
+  str.split('').map(ch => blackboardMap[ch] ?? ch).join('');
+const boldConvert = (str) =>
+  str.split('').map(ch => boldMap[ch] ?? ch).join('');
 const cursiveConvert = (str) =>
   str.split('').map(ch => cursiveMap[ch] ?? ch).join('');
+const frakturConvert = (str) =>
+  str.split('').map(ch => frakturMap[ch] ?? ch).join('');
+const minchoConvert = (str) =>
+  str.split('').map(ch => minchoMap[ch] ?? ch).join('');
+const typewriterConvert = (str) =>
+  str.split('').map(ch => typewriterMap[ch] ?? ch).join('');
 
 const converters = {
-  cursive: cursiveConvert
+  blackboard: blackboardConvert,
+  bold: boldConvert,
+  cursive: cursiveConvert,
+  fraktur: frakturConvert,
+  mincho: minchoConvert,
+  typewriter: typewriterConvert
 };
 
 
@@ -18,7 +38,12 @@ export const data = new SlashCommandBuilder()
       .setDescription('変換モードを選択')
       .setRequired(true)
       .addChoices(
-        { name: '筆記体', value: 'cursive' }
+        { name: '黒板太字', value: 'blackboard' },
+        { name: '太字', value: 'bold' },
+        { name: '筆記体', value: 'cursive' },
+        { name: 'ドイツ文字', value: 'fraktur' },
+        { name: '明朝体', value: 'mincho' },
+        { name: 'タイプライタ体', value: 'typewriter' }
       )
   )
   .addStringOption(option =>
