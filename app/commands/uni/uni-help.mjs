c1c252405c6f0ff1c805d05fae5924440739d7e6
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2025-09-13 11:06:50 +0900

    Update uni-help.mjs

--- app/commands/uni/uni-help.mjs
+++ app/commands/uni/uni-help.mjs
@@ -6,9 +6,27 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
   await interaction.reply({
-    content: `\`\`\`
-test
-\`\`\``,
+    content: `
+**/uni-emoji-count**
+指定した年月のカスタム絵文字使用回数のトップ10（昇順と降順）
+表示までに１～２分かかります
+
+**/uni-gacha**
+ソート、ジャンル、曲目、難易度をランダムに選出
+更新頻度は低め
+
+**/uni-intro**
+自己紹介をする際に使用されたテンプレート
+保存用
+
+**/uni-string**
+アルファベットを好きな字体に変えてくれる
+コピペ目的のために一覧表示なし
+
+**/uni-teamcource**
+チームコースを作ってもらう際に使用されたテンプレート
+保存用
+`,
     ephemeral: true
   });
 }
