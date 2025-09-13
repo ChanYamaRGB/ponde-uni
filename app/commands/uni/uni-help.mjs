501ca9e7098d40d25138d825607ff305995cd101
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2025-09-13 11:09:27 +0900

    Update uni-help.mjs

--- app/commands/uni/uni-help.mjs
+++ app/commands/uni/uni-help.mjs
@@ -7,23 +7,23 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
   await interaction.reply({
     content: `
-**/uni-emoji-count**
+**uni-emoji-count**
 指定した年月のカスタム絵文字使用回数のトップ10（昇順と降順）
 表示までに１～２分かかります
 
-**/uni-gacha**
+**uni-gacha**
 ソート、ジャンル、曲目、難易度をランダムに選出
 更新頻度は低め
 
-**/uni-intro**
+**uni-intro**
 自己紹介をする際に使用されたテンプレート
 保存用
 
-**/uni-string**
+**uni-string**
 アルファベットを好きな字体に変えてくれる
 コピペ目的のために一覧表示なし
 
-**/uni-teamcource**
+**uni-teamcource**
 チームコースを作ってもらう際に使用されたテンプレート
 保存用
 `,
