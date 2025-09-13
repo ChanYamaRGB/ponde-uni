db15177175226be3d3b9a8233601bc087dae2d97
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2025-09-13 10:44:21 +0900

    Update uni-help.mjs

--- app/commands/uni/uni-help.mjs
+++ app/commands/uni/uni-help.mjs
@@ -4,8 +4,11 @@ export const data = new SlashCommandBuilder()
   .setName('uni-help')
   .setDescription('現在使えるコマンドを表示します');
 
-export async function execute(interaction){
-	await interaction.reply(`\`\`\`
+export async function execute(interaction) {
+  await interaction.reply({
+    content: `\`\`\`
 test
-\`\`\``);
+\`\`\``,
+    ephemeral: true
+  });
 }
