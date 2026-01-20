ffed1f142ace8f64f1a25b3ec07ccc18f7ce6720
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2026-01-20 23:48:52 +0900

    Update main.mjs

--- app/main.mjs
+++ app/main.mjs
@@ -83,6 +83,8 @@ client.on("ready", async () => {
 
   await client.user.setActivity('チュウニズム');
 
+  await updateNicknames(client, "あなたのGuildID");
+
   boostDates(client);
   schedule_update(client);
   song_update(client, true);
