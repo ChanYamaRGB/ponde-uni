5342cb722b7ec56f8958ed624b8a5ff8cf7c0204
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2026-04-10 18:20:45 +0900

    Update uni-score.mjs

--- app/commands/uni/uni-score.mjs
+++ app/commands/uni/uni-score.mjs
@@ -264,7 +264,7 @@ function truncateFullWidth(str, maxWidth) {
 
     displayName = truncateFullWidth(displayName, 16);
 
-    const rankStr = padStartFullWidth(`${rank}.`, 4);
+    const rankStr = padStartFullWidth(`${rank}位`, 4);
     const nameStr = padEndFullWidth(displayName, 16);
     const pointStr = padStartFullWidth(`${data.points}`, 6);
     const usableStr = padStartFullWidth(`${data.usable}`, 6);
