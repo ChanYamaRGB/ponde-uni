45e784973fc5f44e9677cd058da36ee817cb2401
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2026-04-30 02:09:27 +0900

    Update uni-score.mjs

--- app/commands/uni/uni-score.mjs
+++ app/commands/uni/uni-score.mjs
@@ -208,7 +208,7 @@ export async function execute(interaction) {
 
   let description = "";
     
-    description += "順位　ユーザー名　　　累計（使用可能）\n\n";
+    description += "__順位　ユーザー名　　　累計（使用可能）__\n\n";
   
   let rank = 1;
 
@@ -225,7 +225,7 @@ export async function execute(interaction) {
     }
 
     description +=
-      `**${rank}位　${displayName}**　${data.points}pt（${data.usable}pt）\n`;
+      `__**${rank}位　${displayName}**　${data.points}pt（${data.usable}pt）__\n`;
     rank++;
   }
 
