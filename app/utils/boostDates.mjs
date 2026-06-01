39de699524e826493b45528400fdd47cbe9b35ad
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2026-06-01 15:48:00 +0900

    Update boostDates.mjs

--- app/utils/boostDates.mjs
+++ app/utils/boostDates.mjs
@@ -34,8 +34,8 @@ async function saveData(channel, data, oldMessage = null) {
 
 export default function schedule_post(client) {
 
-  const targetDates = [2, 5, 16, 29];
-  // const targetDates = [];
+  // const targetDates = [2, 5, 16, 29];
+  const targetDates = [];
   const targetChannelId = "1155482638493171782";
 
   const postOrDelete = async () => {
