6af6e27d7a8eff3c69deaebe424ea0add3426a30
ChanYamaRGB <90698741+ChanYamaRGB@users.noreply.github.com>
2026-06-24 12:26:31 +0900

    Update eventMessages.mjs

--- app/utils/eventMessages.mjs
+++ app/utils/eventMessages.mjs
@@ -3,6 +3,13 @@ const EVENT_DB_CHANNEL_ID = "1509445014906736750";
 
 const EVENTS = [
   {
+    id: "eiketsu", // 英傑対戦
+    start: "2026-04-16",
+    end: "2026-07-01",
+    content:
+`https://cdn.wikiwiki.jp/to/w/chunithmwiki/%E8%8B%B1%E5%82%91%E5%A4%A7%E6%88%A6%E3%82%B3%E3%83%A9%E3%83%9C%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88/::attach/%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E5%91%8A%E7%9F%A5%28%E6%96%B0%29.jpg?rev=931fc00356bba8da68a87c5c77bbdb5e&t=20260504120240`
+  },
+  {
     id: "himehina", // HIMEHINA
     start: "2026-05-14",
     end: "2026-07-01",
@@ -43,6 +50,20 @@ const EVENTS = [
     end: "2026-07-01",
     content:
 `https://info-chunithm.sega.jp/wp-content/uploads/2026/06/fc9905b5323efb956d1465079f567db1.png`
+  },
+  {
+    id: "takopi", // タコピーの原罪
+    start: "2026-07-02",
+    end: "2026-07-29",
+    content:
+`https://cdn.wikiwiki.jp/to/w/chunithmwiki/%E3%82%BF%E3%82%B3%E3%83%94%E3%83%BC%E3%81%AE%E5%8E%9F%E7%BD%AA%E3%82%B3%E3%83%A9%E3%83%9C%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88/::attach/HLKrUA2bMAAWRpR.webp?rev=8a68667f68d6c9274574221a6dd2f0e2&t=20260622120541`
+  },
+  {
+    id: "monsterhunter", // モンスターハンターワイルズ
+    start: "2026-07-02",
+    end: "2026-08-19",
+    content:
+`https://cdn.wikiwiki.jp/to/w/chunithmwiki/%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%8F%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%AF%E3%82%A4%E3%83%AB%E3%82%BA%E3%82%B3%E3%83%A9%E3%83%9C%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88/::attach/HLDPWi2aEAAyHu_.webp?rev=d827e323fee509c385e453d981f09656&t=20260623150516`
   }
 ];
 
@@ -121,9 +142,6 @@ export async function updateEvents(client) {
             await postChannel.messages.fetch(
               dbEntry.data.messageId
             );
-
-          // 編集
-          await targetMsg.edit(event.content);
 
         } catch {
 
